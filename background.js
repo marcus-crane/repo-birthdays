58b5b55095809904b9037a16f2ebbee724471503
Marcus Crane <marcus@utf9k.net>
2021-11-14 17:44:08 +1300

    Convert to manifest v3

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.tabs.executeScript(null, {
-    file: 'birthday.js'
+chrome.action.onClicked.addListener(function (tab) {
+  chrome.scripting.executeScript(null, {
+    files: ['birthday.js']
   })
 })
